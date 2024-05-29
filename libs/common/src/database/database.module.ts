c0505f6e4517340e299d6996d674266ef82a4d88
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-05-29 18:10:56 +0700

    first commit

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    //connect mongoose
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URI'),
+      }),
+      inject: [ConfigService], // error
+    }),
+  ],
+})
+export class DatabaseModule {}
